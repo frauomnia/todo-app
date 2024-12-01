a9bdfcec01477a01db5a8f86729560b5b72e96db
frauomnia <omnia.mohamad@gmail.com>
2024-12-01 22:23:57 +0100

    add toggle feature for complete todo items

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { RiTodoLine } from "react-icons/ri";
 import TodoItems from './TodoItems';
 
 const Todo = () => {
 
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")) : []);
   const inputRef = useRef();
 
   const add = ()=> {
@@ -29,21 +29,36 @@ const Todo = () => {
     })
   }
 
+  const toggle = (id)=> {
+    setTodoList((prevTodos)=>{
+      return prevTodos.map((todo)=> {
+        if (todo.id === id) {
+          return {...todo, isComplete: !todo.isComplete}
+        }
+        return todo;
+      })
+    })
+  }
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todoList)
+  )},[todoList])
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
       
     {/* --------- title ---------- */}
 
       <div className='flex items-center mt-7 gap-2'>
-        <RiTodoLine size={21} />
-        <h1 className='text-3xl font-semibold'>To-Do list</h1>
+        <RiTodoLine size={21} color='#134e4a' />
+        <h1 className='text-3xl font-semibold text-teal-900'>To-Do list</h1>
       </div>
 
     {/* --------- input box ---------- */}
 
       <div className='flex items-center my-7 bg-orange-500 rounded-full'>
         <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-teal-900' type='text' placeholder='Add your task'/>
-        <button onClick={add} className='border-none rounded-full bg-teal-900 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <button onClick={add} className='border-none rounded-full bg-teal-900 w-20 h-14 text-white text-lg font-medium cursor-pointer'>+</button>
       </div>
 
     {/* --------- todo list ---------- */}
@@ -55,7 +70,8 @@ const Todo = () => {
           text={item.text} 
           id={item.id} 
           isComplete={item.isComplete}
-          deleteTodo={deleteTodo} />
+          deleteTodo={deleteTodo} 
+          toggle={toggle} />
         })}
       </div>
 
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -4,16 +4,16 @@ import { ImCheckboxUnchecked } from "react-icons/im";
 import { TiDelete } from "react-icons/ti";
 
 
-const TodoItems = ({text, id, isComplete, deleteTodo}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-      <div className='flex flex-1 items-center cursor-pointer'>
-        <ImCheckboxChecked size={17}/>
-        <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
-        {/* <ImCheckboxUnchecked /> */}
+
+      <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
+        {isComplete ? < ImCheckboxChecked size={17}/> : <ImCheckboxUnchecked size={17}/>}
+        <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>{text}</p>
       </div>
       <TiDelete size={20} className='cursor-pointer' onClick={()=>{deleteTodo(id)}} />
-
+    
     </div>
   )
 }
